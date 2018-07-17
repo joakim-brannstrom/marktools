531988fceac1bc72eac18b5a233132206ddf4596
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-07-17 17:26:47 +0200

    Refactor source code

--- source/app.d
+++ source/app.d
@@ -3,85 +3,33 @@ Copyright: Copyright (c) 2018, Joakim Brännström. All rights reserved.
 License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
 Author: Joakim Brännström (joakim.brannstrom@gmx.com)
 */
+module app;
 
 int main(string[] args) {
     import std.stdio : writefln;
+    import markdown;
+    import pdf;
 
     auto conf = cliParser(args);
     if (!conf.valid)
         return 1;
 
     alias HandleFn = int function(const Config);
+
     HandleFn[Config.Format] handle;
+
     handle[Config.Format.commonmark] = &toMarkdown;
+    handle[Config.Format.pdf] = &toPdf;
 
     if (auto v = conf.dstFmt in handle) {
+        writefln("Converting '%s' (%s) to '%s' (%s)", conf.src, conf.srcFmt,
+                conf.dst, conf.dstFmt);
         return (*v)(conf);
     }
 
     writefln("Destination '%s' not supported", conf.dstFmt);
     writefln("Supported are %s", handle.byKey);
     return 1;
-}
-
-int toMarkdown(const Config conf) {
-    import std.conv : to;
-    import std.process : execute;
-    import std.stdio : writeln, File;
-
-    auto pass1 = execute(["pandoc", "-s", "-S", "-f" ~ conf.srcFmt.to!string,
-            "-t" ~ conf.dstFmt.to!string, conf.src]);
-    if (pass1.status != 0) {
-        writeln(pass1.output);
-        return pass1.status;
-    }
-
-    auto clean_output = pass1.output.markdownCleanup;
-
-    auto fout = File(conf.dst, "w");
-    fout.writefln("[Original Source](%s)", conf.src);
-    fout.write(clean_output);
-
-    return 0;
-}
-
-char[] markdownCleanup(const(char)[] input) {
-    char[] output;
-    size_t j;
-
-    int column;
-    for (size_t i = 0; i < input.length; i++) {
-        char c = input[i];
-
-        switch (c) {
-        case '\t':
-            while ((column & 7) != 7) {
-                output ~= ' ';
-                j++;
-                column++;
-            }
-            c = ' ';
-            column++;
-            break;
-
-        case '\r':
-        case '\n':
-            while (j && output[j - 1] == ' ')
-                j--;
-            output = output[0 .. j];
-            column = 0;
-            break;
-
-        default:
-            column++;
-            break;
-        }
-        output ~= c;
-        j++;
-    }
-    while (j && output[j - 1] == ' ')
-        j--;
-    return output[0 .. j];
 }
 
 struct Config {
@@ -96,17 +44,20 @@ struct Config {
 
     /// Source format parameters to pandoc
     Format srcFmt = Format.html;
-    string src;
+    string[] src;
 
     /// Destination format
     Format dstFmt = Format.commonmark;
     string dst;
 }
 
+private:
+
 Config cliParser(string[] args) {
     import std.format : format;
     import std.path : absolutePath;
-    import std.stdio : writeln;
+    import std.stdio : writeln, writefln;
+    import std.traits : EnumMembers;
 
     Config conf;
 
@@ -123,14 +74,12 @@ Config cliParser(string[] args) {
         // dfmt on
 
         conf.valid = () {
-            if (args.length == 3) {
-                conf.src = args[1];
-                conf.dst = args[2].absolutePath;
+            if (args.length >= 3) {
+                conf.src = args[1 .. $ - 1];
+                conf.dst = args[$ - 1].absolutePath;
                 return !help_info.helpWanted;
             } else if (args.length < 3)
-                writeln("Missing arguments SOURCE and DESTINATION");
-            else if (args.length > 3)
-                writeln("Too many arguments");
+                writeln("Missing arguments [SOURCE] and DESTINATION");
             return false;
         }();
     } catch (std.getopt.GetOptException e) {
@@ -145,8 +94,9 @@ Config cliParser(string[] args) {
         import std.format : format;
         import std.path : baseName;
 
-        defaultGetoptPrinter(format("usage: %s [options] SOURCE DESTINATION\n",
+        defaultGetoptPrinter(format("usage: %s [options] [SOURCE] DESTINATION\n",
                 args[0].baseName), help_info.options);
+        writefln("Supported formats %s", [EnumMembers!(Config.Format)]);
     }
 
     if (!conf.valid) {
---
+++ source/markdown.d
@@ -0,0 +1,70 @@
+/**
+Copyright: Copyright (c) 2018, Joakim Brännström. All rights reserved.
+License: $(LINK2 http://www.boost.org/LICENSE_1_0.txt, Boost Software License 1.0)
+Author: Joakim Brännström (joakim.brannstrom@gmx.com)
+*/
+module markdown;
+
+import app : Config;
+
+int toMarkdown(const Config conf) {
+    import std.conv : to;
+    import std.process : execute;
+    import std.stdio : writeln, File;
+
+    auto pass1 = execute(["pandoc", "-s", "-S", "-f" ~ conf.srcFmt.to!string,
+            "-t" ~ conf.dstFmt.to!string] ~ conf.src);
+    if (pass1.status != 0) {
+        writeln(pass1.output);
+        return pass1.status;
+    }
+
+    auto clean_output = pass1.output.markdownCleanup;
+
+    auto fout = File(conf.dst, "w");
+    fout.writefln("[Original Source](%s)", conf.src);
+    fout.write(clean_output);
+
+    return 0;
+}
+
+private:
+
+char[] markdownCleanup(const(char)[] input) {
+    char[] output;
+    size_t j;
+
+    int column;
+    for (size_t i = 0; i < input.length; i++) {
+        char c = input[i];
+
+        switch (c) {
+        case '\t':
+            while ((column & 7) != 7) {
+                output ~= ' ';
+                j++;
+                column++;
+            }
+            c = ' ';
+            column++;
+            break;
+
+        case '\r':
+        case '\n':
+            while (j && output[j - 1] == ' ')
+                j--;
+            output = output[0 .. j];
+            column = 0;
+            break;
+
+        default:
+            column++;
+            break;
+        }
+        output ~= c;
+        j++;
+    }
+    while (j && output[j - 1] == ' ')
+        j--;
+    return output[0 .. j];
+}
