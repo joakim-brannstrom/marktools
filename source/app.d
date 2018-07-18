23659bae72c47bbbcdb6e43208ec1992cef8e97e
Joakim Brannstrom <joakim.brannstrom@gmx.com>
2018-07-18 15:58:39 +0200

    Add convenient groups

--- source/app.d
+++ source/app.d
@@ -5,14 +5,30 @@ Author: Joakim Brännström (joakim.brannstrom@gmx.com)
 */
 module app;
 
-int main(string[] args) {
-    import std.stdio : writefln;
-    import marktool.markdown;
-    import marktool.pdf;
+import std.exception : collectException;
 
+int main(string[] args) {
     auto conf = cliParser(args);
     if (!conf.valid)
         return 1;
+
+    alias Fn = int function(const Config);
+    Fn[Config.Command] cmds;
+
+    cmds[Config.Command.standard] = &commandStandard;
+    cmds[Config.Command.install] = &commandInstall;
+
+    if (auto v = conf.cmd in cmds) {
+        return (*v)(conf);
+    }
+
+    return 1;
+}
+
+int commandStandard(const Config conf) {
+    import std.stdio : writefln;
+    import marktool.markdown;
+    import marktool.pdf;
 
     alias HandleFn = int function(const Config);
 
@@ -31,6 +47,34 @@ int main(string[] args) {
     return 1;
 }
 
+int commandInstall(const Config conf) {
+    import std.algorithm : map;
+    import std.conv : text;
+    import std.file : thisExePath, symlink, exists, remove;
+    import std.path : dirName, buildPath;
+    import std.stdio : writeln, writefln;
+    import std.traits : EnumMembers;
+
+    const string this_ = thisExePath;
+    const string base = this_.dirName;
+
+    foreach (def_; [EnumMembers!PreConfigDefault].map!text) {
+        const target = buildPath(base, def_);
+
+        try {
+            if (exists(target))
+                remove(target);
+            symlink(this_, target);
+            writefln("Created symlink '%s' -> '%s'", this_, target);
+        } catch (Exception e) {
+            writeln(e.msg).collectException;
+            writefln("Unable to create symlink '%s' -> '%s'", this_, target).collectException;
+        }
+    }
+
+    return 0;
+}
+
 struct Config {
     /// False if the config is invalid in some way. Do not use the data. Just exit.
     bool valid;
@@ -40,6 +84,13 @@ struct Config {
         pdf,
         commonmark
     }
+
+    enum Command {
+        standard,
+        install,
+    }
+
+    Command cmd;
 
     /// Source format parameters to pandoc
     Format srcFmt = Format.html;
@@ -52,54 +103,110 @@ struct Config {
 
 private:
 
+/// Pre-configured defaults that make it convenient for the user.
+enum PreConfigDefault {
+    html2markdown,
+    markdown2pdf,
+}
+
 Config cliParser(string[] args) {
+    import std.conv : to;
+    import std.file : thisExePath;
     import std.format : format;
-    import std.path : absolutePath;
+    import std.path : absolutePath, baseName;
     import std.stdio : writeln, writefln;
     import std.traits : EnumMembers;
 
-    Config conf;
+    static void genericParser(string[] args, ref Config conf) {
+        static import std.getopt;
 
-    static import std.getopt;
+        std.getopt.GetoptResult help_info;
+        try {
+            bool install;
+            // dfmt off
+            help_info = std.getopt.getopt(args, std.getopt.config.passThrough,
+                std.getopt.config.keepEndOfOptions,
+                "f", format("format of the source (default: %s)", Config.init.srcFmt), &conf.srcFmt,
+                "t", format("format of the destination (default: %s)", Config.init.dstFmt), &conf.dstFmt,
+                "install", "create symlinks for convenient defaults", &install,
+                );
+            // dfmt on
 
-    std.getopt.GetoptResult help_info;
-    try {
-        // dfmt off
-        help_info = std.getopt.getopt(args, std.getopt.config.passThrough,
-            std.getopt.config.keepEndOfOptions,
-            "f", format("format of the source (default: %s)", Config.init.srcFmt), &conf.srcFmt,
-            "t", format("format of the destination (default: %s)", Config.init.dstFmt), &conf.dstFmt,
-            );
-        // dfmt on
+            conf.valid = () {
+                if (install) {
+                    conf.cmd = Config.Command.install;
+                    return true;
+                } else if (args.length >= 3) {
+                    conf.src = args[1 .. $ - 1];
+                    conf.dst = args[$ - 1].absolutePath;
+                    return !help_info.helpWanted;
+                } else if (args.length < 3)
+                    writeln("Missing arguments [SOURCE] and DESTINATION");
+                return false;
+            }();
+        } catch (std.getopt.GetOptException e) {
+            // unknown option
+            writeln(e.msg);
+        } catch (Exception e) {
+            writeln(e.msg);
+        }
 
+        void printHelp() {
+            import std.getopt : defaultGetoptPrinter;
+            import std.format : format;
+            import std.path : baseName;
+
+            defaultGetoptPrinter(format("usage: %s [options] [SOURCE] DESTINATION\n",
+                    args[0].baseName), help_info.options);
+            writefln("Supported formats %s", [EnumMembers!(Config.Format)]);
+        }
+
+        if (!conf.valid) {
+            printHelp;
+        }
+    }
+
+    static void html2Markdown(string[] args, ref Config conf) {
+        conf.srcFmt = Config.Format.html;
+        conf.dstFmt = Config.Format.commonmark;
+
+        conf.valid = () {
+            if (args.length == 3) {
+                conf.src = [args[1]];
+                conf.dst = args[2].absolutePath;
+                return true;
+            }
+            return false;
+        }();
+    }
+
+    static void markdown2Pdf(string[] args, ref Config conf) {
+        conf.srcFmt = Config.Format.commonmark;
+        conf.dstFmt = Config.Format.pdf;
         conf.valid = () {
             if (args.length >= 3) {
                 conf.src = args[1 .. $ - 1];
                 conf.dst = args[$ - 1].absolutePath;
-                return !help_info.helpWanted;
-            } else if (args.length < 3)
-                writeln("Missing arguments [SOURCE] and DESTINATION");
+                return true;
+            }
             return false;
         }();
-    } catch (std.getopt.GetOptException e) {
-        // unknown option
-        writeln(e.msg);
+    }
+
+    Config conf;
+
+    alias Fn = void function(string[] args, ref Config);
+    Fn[PreConfigDefault] cmds;
+    cmds[PreConfigDefault.html2markdown] = &html2Markdown;
+    cmds[PreConfigDefault.markdown2pdf] = &markdown2Pdf;
+
+    try {
+        if (auto v = thisExePath.baseName.to!(PreConfigDefault) in cmds)
+            (*v)(args, conf);
+        else
+            genericParser(args, conf);
     } catch (Exception e) {
-        writeln(e.msg);
-    }
-
-    void printHelp() {
-        import std.getopt : defaultGetoptPrinter;
-        import std.format : format;
-        import std.path : baseName;
-
-        defaultGetoptPrinter(format("usage: %s [options] [SOURCE] DESTINATION\n",
-                args[0].baseName), help_info.options);
-        writefln("Supported formats %s", [EnumMembers!(Config.Format)]);
-    }
-
-    if (!conf.valid) {
-        printHelp;
+        genericParser(args, conf);
     }
 
     return conf;
